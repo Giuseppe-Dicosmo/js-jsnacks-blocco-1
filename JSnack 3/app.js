82725301928bbf8efee4a057d39c963eba2b9fa1
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-11 17:10:53 +0100

    JSnack 3

---
+++ JSnack 3/app.js
@@ -0,0 +1,38 @@
+// const numeri = [];
+// let somma = 0;
+
+// for (let i = 0; i < 5; i++) {
+//   const numeroUser = parseInt(prompt('Inserisci un numero'));
+//   numeri.push(numeroUser)
+// }
+// console.log(numeri)
+
+// for (let i = 0; i < 5; i++) {
+//   somma = somma + numeri[i];
+//   console.log(somma)
+// }
+
+// ----------------------------------------------------------
+
+// let somma = 0;
+
+// for (let i = 0; i < 5; i++) {
+//   const numeroUser = parseInt(prompt('Inserisci un numero'));
+//   somma = somma + numeroUser;
+//   console.log(somma)
+// }
+
+// ----------------------------------------------------------
+
+let somma = 0;
+let contatore = 0;
+while (contatore <= 5) {
+  const numeroUser = parseInt(prompt('Inserisci un numero'));
+  somma = somma + numeroUser;
+  console.log(somma)
+  contatore++
+}
+
+
+
+
